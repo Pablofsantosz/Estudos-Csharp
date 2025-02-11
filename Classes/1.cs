ad22e7a6f9ac11404b5aebd475f2e7c44ecd8cf2
Pablo Felipe <125268936+Pablofsantosz@users.noreply.github.com>
2025-02-11 18:54:10 +0000

    commit

---
+++ Classes/1.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Globalization;
+
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa P1, P2;
+            P1 = new Pessoa();
+            P2 = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa");
+            Console.Write("Nome :");
+            P1.Nome = Console.ReadLine();
+            Console.Write("Idade :");
+            P1.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa");
+            Console.WriteLine("Nome :");
+            P2.Nome = Console.ReadLine();
+            Console.WriteLine("Idade :");
+            P2.Idade = int.Parse(Console.ReadLine());
+
+            if(P1.Idade > P2.Idade)
+            {
+                Console.WriteLine("Pessoa mais velha :" + P1.Nome);
+
+            }
+            else if(P2.Idade > P1.Idade)
+            {
+                Console.WriteLine("Pessoa mais velha :" + P2.Nome);
+            }
+            else
+            {
+                Console.WriteLine("Idade iguais");
+            }
+
+
+
+        }
+    }
+}
